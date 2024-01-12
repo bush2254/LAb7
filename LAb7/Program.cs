4086c55726e35a89c4a282c81ef50ba4e6ca51dc
BushevAndrey <chhufudud@gmail.com>
2024-01-12 17:49:12 +0300

    Добавьте файлы проекта.

---
+++ LAb7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s = 1;
+            for(int i = 1; i <= 10; i++)
+            {
+                double p = 1;
+                for (int j = 1; j <= 5; j++) 
+                {
+                    p *= (Abs(Sin(i) * Cos(j))) /( i + j);
+                }
+                s *= p;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
